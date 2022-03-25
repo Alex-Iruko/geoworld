2ff0e31021be2c68bfdd4748e4be7f72abf584ab
Quentinho199 <97668671+Quentinho199@users.noreply.github.com>
2022-03-25 11:44:11 +0100

    Add files via upload

--- inc/manager-db.php
+++ inc/manager-db.php
@@ -30,11 +30,27 @@ require_once 'connect-db.php';
  * 
  * @return array tableau d'objets (des pays)
  */
+function getContinent()
+{
+    // pour utiliser la variable globale dans la fonction
+    global $pdo;
+    $query = 'SELECT DISTINCT Continent FROM Country Order by continent ASC';
+    $prep = $pdo->prepare($query);
+    try { 
+        $result = $pdo->query($query)->fetchAll(); 
+        return $result;
+      }
+      catch ( Exception $e ) {
+        die ("erreur dans la requete ".$e->getMessage());
+      }
+}
 function getCountriesByContinent($continent)
 {
     // pour utiliser la variable globale dans la fonction
     global $pdo;
-    $query = 'SELECT * FROM Country WHERE Continent = :cont;';
+    $query = 'SELECT country.id as id,country.name as Name, city.Name as Capitale, country.Population as Population, Code2 as Code2 
+    FROM country, city
+    WHERE country.Capital=city.id AND country.Continent=:cont;';
     $prep = $pdo->prepare($query);
     // on associe ici (bind) le paramètre (:cont) de la req SQL,
     // avec la valeur reçue en paramètre de la fonction ($continent)
@@ -48,6 +64,29 @@ function getCountriesByContinent($continent)
     // on retourne un tableau d'objets (car spécifié dans connect-db.php)
     return $prep->fetchAll();
 }
+function getDetailPays($id){
+  global $pdo;
+  $query ="SELECT country.Code as Code,country.Name as Nom,country.Continent as Continent, country.Region as Région,country.SurfaceArea as Surface, country.IndepYear as AnneeI, country.Population as Pop,
+   country.LifeExpectancy as EspVie,country.LocalName as NomL, country.GovernmentForm as TypeG
+  ,country.HeadOfState as ChefE,city.Name as Capitale, Code2 as Code2
+  FROM country, city
+  WHERE country.Capital=city.id and country.id=:id;";
+  $prep = $pdo->prepare($query);
+  $prep->bindValue(':id',$id, PDO::PARAM_INT);
+  $prep->execute();
+  return $prep->fetchAll();
+}
+function getLangue($id){
+  global $pdo;
+  $query ="SELECT language.Name as Langue
+  FROM country, city, countrylanguage,language
+  WHERE country.Capital=city.id and country.id=:id and country.id=countrylanguage.idCountry and countrylanguage.idLanguage=language.id
+  ORDER BY countrylanguage.IsOfficial;";
+  $prep = $pdo->prepare($query);
+  $prep->bindValue(':id',$id, PDO::PARAM_INT);
+  $prep->execute();
+  return $prep->fetchAll();
+}
 
 /**
  * Obtenir la liste des pays
@@ -60,24 +99,3 @@ function getAllCountries()
     $query = 'SELECT * FROM Country;';
     return $pdo->query($query)->fetchAll();
 }
-function getContinent()
-{
-    global $pdo;
-    $query =' SELECT DISTINCT Continent FROM country;';
-    return $pdo-> query($query)->fetchAll();
-}
-function getCapital($id){
-    global $pdo;
-    if(empty($id)){
-        echo("Pas de Capital");
-    }
-    else {
-        $query = 'SELECT city.Name FROM City WHERE id = :id;';
-        $prep = $pdo->prepare($query);
-        $prep->bindValue(':id', $id, PDO::PARAM_STR);
-        $prep->execute();
-
-        return $prep->fetch()->Name;
-    }
-}
-?>
